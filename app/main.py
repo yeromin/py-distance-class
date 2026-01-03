7042f28623969bb2a919c27e16d401ddd45c5982
dima.yeromin <dima.yeromin@gmail.com>
2026-01-03 15:06:27 +0100

    Fixes according to the requirements list

--- app/main.py
+++ app/main.py
@@ -1,72 +1,82 @@
+from __future__ import annotations
+
+from types import NotImplementedType
+
+
+Number = int | float
+
+
 class Distance:
-    def __init__(self, km):
+    def __init__(self, km: Number) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Distance | Number) -> Distance | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return Distance(self.km + other_km)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Distance | Number) -> Distance | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         self.km += other_km
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: Number) -> Distance | NotImplementedType:
         if isinstance(other, Distance):
             raise TypeError("Distance can only be multiplied by a number")
         if not isinstance(other, (int, float)):
             return NotImplemented
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Number) -> Distance | NotImplementedType:
         if isinstance(other, Distance):
             raise TypeError("Distance can only be divided by a number")
         if not isinstance(other, (int, float)):
             return NotImplemented
         return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other):
+    def __eq__(self, other: Distance | Number) -> bool | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km == other_km
 
-    def __lt__(self, other):
+    def __lt__(self, other: Distance | Number) -> bool | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km < other_km
 
-    def __le__(self, other):
+    def __le__(self, other: Distance | Number) -> bool | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km <= other_km
 
-    def __gt__(self, other):
+    def __gt__(self, other: Distance | Number) -> bool | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km > other_km
 
-    def __ge__(self, other):
+    def __ge__(self, other: Distance | Number) -> bool | NotImplementedType:
         other_km = self._get_number_or_distance(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km >= other_km
 
     @staticmethod
-    def _get_number_or_distance(value):
+    def _get_number_or_distance(
+        value: Distance | Number,
+    ) -> Number | NotImplementedType:
         if isinstance(value, Distance):
             return value.km
         if isinstance(value, (int, float)):
